1772cea156238879af65853f9906d12b886f6fb5
Gabe Gil <gilgabe214@gmail.com>
2020-11-10 23:35:42 -0500

    initial commit

---
+++ src/components/search.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Search = () => (
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <input className="form-control search-input" type="text" placeholder="City Name"/>
+    </div>
+)
+
+export default Search
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Search from "../components/search"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  <SEO title="Home" />
+    <Container>
+      <Row className="justify-content-md-center" xs={12}>
+        <Col xs={8}>
+          <h3 className="search-header">Find the Pulse of your city</h3>
+          <Search />
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
