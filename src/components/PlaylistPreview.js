ce972ac60e13906b91891d22ba2edea4a6e99cf4
Gabe Gil <gilgabe214@gmail.com>
2021-03-29 23:01:37 -0400

    create components for user to review recommended songs before adding

---
+++ src/components/PlaylistPreview.js
@@ -0,0 +1,17 @@
+import React, { useContext, useState } from "react";
+
+function PlaylistPreview(props) {
+
+
+  return (
+    <div className="playlist-preview">
+      <h1>Here are your recommendend songs</h1>
+      <div>
+        {
+          //List of Recommended Songs
+        }
+      </div>
+    </div>
+  );
+}
+export default PlaylistPreview;
---
+++ src/pages/review.js
@@ -0,0 +1,25 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Layout from "../components/layout"
+import PlaylistPreview from "../components/PlaylistPreview"
+import SEO from "../components/seo"
+
+const ReviewPage = () => (
+
+  <Layout>
+  <SEO title="Playlist Overview" />
+    <Container>
+      <Row className="justify-content-md-center" xs={12}>
+        <Col xs={8}>
+          <PlaylistPreview />
+        </Col>
+      </Row>
+    </Container>
+  </Layout>
+)
+
+export default ReviewPage
