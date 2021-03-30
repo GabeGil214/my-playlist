edd66ca27a51d53cbd26d024864fbac42a3f0dcf
Gabe Gil <gilgabe214@gmail.com>
2021-03-29 23:00:45 -0400

    create context for playlist info

---
+++ src/Reducers/playlistReducer.js
@@ -0,0 +1,33 @@
+import React, { useReducer, createContext } from 'react';
+
+export const PlaylistContext = createContext({})
+
+const initialPlaylistState = {
+    trackIDs: [],
+};
+
+export const playlistReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TRACK':
+            return {
+                trackIDs: [...state],
+            };
+        case 'REMOVE_TRACK':
+            return {
+                trackIDs: [...state]
+            };
+        default:
+            return state;
+    }
+};
+
+
+export const PlaylistContextProvider = props => {
+  const [state, dispatch] = useReducer(playlistReducer, initialPlaylistState);
+
+  return (
+    <PlaylistContext.Provider value={[state, dispatch]}>
+      {props.children}
+    </PlaylistContext.Provider>
+  )
+}
