885bba08182de9a817f7233d8afb5c383dfdf4c6
Gabe Gil <gilgabe214@gmail.com>
2021-07-17 16:32:02 -0400

    changes to playlistContainer url path

--- src/pages/playlist.js
+++ src/pages/playlist.js
@@ -15,7 +15,7 @@ const PlaylistPage = () => (
     <Container>
       <Row className="justify-content-sm-center">
         <Router>
-          <PlaylistContainer path="/:code"/>
+          <PlaylistContainer path=":code"/>
         </Router>
       </Row>
     </Container>
