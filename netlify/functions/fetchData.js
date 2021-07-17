f402cf86e3009d1fde3d4927ed35dfbe5e4f00b2
Gabe Gil <gilgabe214@gmail.com>
2021-07-17 14:47:28 -0400

    add netlify function to protect secret key

---
+++ netlify/functions/fetchData.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  //Securely access environment variables here
+  try {
+    const { data } = event.queryStringParameters;
+    const response = await axios.post(`https://accounts.spotify.com/api/token`, qs.stringify(data),
+      {
+        headers: {
+          'Content-Type' : 'application/x-www-form-urlencoded',
+          'Authorization' : 'Basic ' + btoa(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET)
+        }
+      })
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    }
+  } catch (error) {
+    statusCode: 404,,
+    body: error.toString()
+  }
+};
