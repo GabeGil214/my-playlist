81a7174618bab062d17693087cd1051991703fda
Gabe Gil <gilgabe214@gmail.com>
2021-08-09 21:43:10 -0400

    convert netlify function to async function

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -2,32 +2,34 @@ const axios = require("axios");
 const qs = require("qs");
 const btoa = require("btoa");
 
-exports.handler = function (event, context) {
+exports.handler = async function (event, context) {
   //Securely access environment variables here
-  const { code } = event.queryStringParameters;
-  const data = {
-     grant_type: 'authorization_code',
-     code: code,
-     redirect_uri: 'http://mycustomplaylist.com/playlist'
-  }
-  axios.post(`https://accounts.spotify.com/api/token`, qs.stringify(data),
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization' : 'Basic ' + btoa(process.env.GATSBY_CLIENT_ID + ':' + process.env.CLIENT_SECRET)
-      }
+  try {
+    const { code } = event.queryStringParameters;
+    const data = {
+       grant_type: 'authorization_code',
+       code: code,
+       redirect_uri: 'http://mycustomplaylist.com/playlist'
     }
-  )
-  .then(response => {
+
+    const response = await axios.post(`https://accounts.spotify.com/api/token`, qs.stringify(data),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization' : 'Basic ' + btoa(process.env.GATSBY_CLIENT_ID + ':' + process.env.CLIENT_SECRET)
+        }
+      }
+    )
+
     return {
       statusCode: 200,
-      body: response.toString()
+      body: JSON.stringify(response)
     }
-  })
-  .catch(error => {
-    return {
-      statusCode: 404,
-      body: error.toString()
-    }
-  })
+    
+  } catch(error) {
+      return {
+        statusCode: 404,
+        body: error.toString()
+      }
+  }
 };
