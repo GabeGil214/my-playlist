6c43ddb5f193905b75f95ac5da1fb782afa9730b
Gabe Gil <gilgabe214@gmail.com>
2021-08-18 21:33:48 -0400

    replace btoa with Buffer method

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -1,10 +1,14 @@
 const axios = require("axios");
 const qs = require("qs");
-const btoa = require("btoa");
+const Buffer = require("buffer")
 
 exports.handler = async function (event, context) {
   //Securely access environment variables here
   try {
+
+    const apiString = process.env.GATSBY_CLIENT_ID + ':' + process.env.CLIENT_SECRET
+
+    Buffer.from(apiString).toString('base64')
 
     const { code } = event.queryStringParameters
     const data = {
@@ -17,7 +21,7 @@ exports.handler = async function (event, context) {
         {
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization' : 'Basic ' + btoa(process.env.GATSBY_CLIENT_ID + ':' + process.env.CLIENT_SECRET)
+            'Authorization' : 'Basic ' + apiString
           }
         })
 
